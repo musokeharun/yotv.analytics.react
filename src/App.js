06c6270dadf3e20d0a6b3ec84ff60bdf3a59821a
Haruna H <musokeharun@gmail.com>
2021-07-05 21:29:38 +0300

    Update for sync

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from "react";
-import {HashRouter, Switch, Route} from "react-router-dom";
+import React, {Fragment} from "react";
+import {HashRouter, Switch, Route, Redirect} from "react-router-dom";
 import "jquery/dist/jquery.min";
 import "popper.js/dist/popper.min";
 import "bootstrap/dist/js/bootstrap.bundle";
@@ -7,25 +7,40 @@ import "./assets/light.css";
 import "./user.css"
 
 import Sidebar from "./layout/sidebar";
-import Topbar from "./layout/topbar";
+import TopBar from "./layout/topbar";
 import Footer from "./layout/footer";
 import Settings from "./pages/settings";
+import AdminLogin from "./modules/admin/login";
+import PartnerLogin from "./modules/partner/login";
 
 function App() {
+    const user = undefined;
+
     return (
         <HashRouter>
-            <Sidebar/>
-            <div className="main">
-                <Topbar/>
-                <main className="content">
-                    <div className="container-fluid p-0">
-                        <Switch>
-                            <Route path={"/settings"} exact render={props => <Settings {...props} />}/>
-                        </Switch>
-                    </div>
-                </main>
-                <Footer/>
-            </div>
+            <Switch>
+                <Route path={"/admin/login"} rend={props => <AdminLogin {...props}/>}/>
+                <Route path={"/partner/login"} rend={props => <PartnerLogin {...props}/>}/>
+                <Route path={"/"} render={props => {
+                    if (!user) return <Redirect to={"/partner/login"}/>
+                    return (
+                        <Fragment>
+                            <Sidebar/>
+                            <div className="main">
+                                <TopBar/>
+                                <main className="content">
+                                    <div className="container-fluid p-0">
+                                        <Switch>
+                                            <Route path={"/settings"} exact render={props => <Settings {...props} />}/>
+                                        </Switch>
+                                    </div>
+                                </main>
+                                <Footer/>
+                            </div>
+                        </Fragment>
+                    );
+                }}/>
+            </Switch>
         </HashRouter>
     );
 }
---
+++ src/modules/partner/login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PartnerLogin = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default PartnerLogin;
